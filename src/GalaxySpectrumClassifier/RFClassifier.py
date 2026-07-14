53813ab3b7590435439c0cd679cd5d241711300a
Harald Mack <harald.mack@protonmail.com]>
2026-07-14 19:09:38 +0200

    add RFClassifier base

---
+++ src/GalaxySpectrumClassifier/RFClassifier.py
@@ -0,0 +1,18 @@
+from .base import ModelBase
+
+
+class RFClassifier(ModelBase):
+    def __init__(self, ...):
+        pass
+
+    def train(self, ...):
+        pass
+
+    def validate(self, ...):
+        pass
+
+    def test(self,...):
+        pass
+
+    def predict(self, input):
+        pass
