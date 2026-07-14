4dd470cc039f2ad8246da31d0f81003d5b62626c
Harald Mack <harald.mack@protonmail.com]>
2026-07-14 19:09:14 +0200

    add basic GBClassifier skeleton

---
+++ src/GalaxySpectrumClassifier/GBClassifier.py
@@ -0,0 +1,18 @@
+from .base import ModelBase
+
+
+class GBClassifier(ModelBase):
+    def __init__(self, ...):
+        pass
+
+    def train(self, ...):
+        pass
+
+    def validate(self, ...):
+        pass
+
+    def test(self,...):
+        pass
+
+    def predict(self, input):
+        pass
