c26e063f52eb675181d8485c5f95465ce0fa2664
Harald Mack <harald.mack@protonmail.com>
2026-07-17 17:28:22 +0200

    add base protocol structure

---
+++ src/GalaxySpectrumClassifier/base.py
@@ -0,0 +1,29 @@
+from typing import Any, Self, Protocol, runtime_checkable
+
+
+@runtime_checkable
+class Configurable(Protocol):
+    @classmethod
+    def from_config(cls, cfg: dict[str, Any]) -> Self: ...
+
+
+class Predicable(Configurable, Protocol):
+    def predict(self, data, *args, **kwargs): ...
+
+    def predict_proba(self, data, *args, **kwargs): ...
+
+    def __call__(self, data, *args, **kwargs): ...
+
+
+class Trainable(Predicable, Protocol):
+    def fit(self, data, *args, **kwargs): ...
+
+
+class Trainer(Configurable, Protocol):
+    def fit(self, dataset, **kwargs): ...
+
+    def train(self, dataset, **kwargs): ...
+
+    def validate(self, dataset, **kwargs): ...
+
+    def test(self, dataset, **kwargs): ...
