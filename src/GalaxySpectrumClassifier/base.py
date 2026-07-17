4366d29b0d7ace7d9a65d25f5309adccf29c077c
Harald Mack <harald.mack@protonmail.com>
2026-07-17 17:30:13 +0200

    update base protocols

--- src/GalaxySpectrumClassifier/base.py
+++ src/GalaxySpectrumClassifier/base.py
@@ -7,6 +7,7 @@ class Configurable(Protocol):
     def from_config(cls, cfg: dict[str, Any]) -> Self: ...
 
 
+@runtime_checkable
 class Predicable(Configurable, Protocol):
     def predict(self, data, *args, **kwargs): ...
 
@@ -15,10 +16,12 @@ class Predicable(Configurable, Protocol):
     def __call__(self, data, *args, **kwargs): ...
 
 
+@runtime_checkable
 class Trainable(Predicable, Protocol):
     def fit(self, data, *args, **kwargs): ...
 
 
+@runtime_checkable
 class Trainer(Configurable, Protocol):
     def fit(self, dataset, **kwargs): ...
 
