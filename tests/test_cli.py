2696ca09facef8b4eac6d06e9311455eafb39a79
Harald Mack <harald.mack@protonmail.com>
2026-07-17 11:13:54 +0200

    remove old stuff

--- tests/test_cli.py
+++
@@ -1,9 +0,0 @@
-from GalaxySpectrumClassifier.__main__ import main
-
-from click.testing import CliRunner
-
-
-def test_GalaxySpectrumClassifier_cli():
-    runner = CliRunner()
-    result = runner.invoke(main, ())
-    assert result.exit_code == 0
