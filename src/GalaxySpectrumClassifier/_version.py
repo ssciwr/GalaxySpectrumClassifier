1114048eced42c760725a0cbc1fa4ec2e66a723b
Harald Mack <harald.mack@protonmail.com]>
2026-08-02 13:44:08 +0200

    update version

--- src/GalaxySpectrumClassifier/_version.py
+++ src/GalaxySpectrumClassifier/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "0.0.post45+ga8284f6c5.d20260802"
-__version_tuple__ = version_tuple = (0, 0, "post45", "ga8284f6c5.d20260802")
+__version__ = version = "0.0.post48+g00a2786c9.d20260802"
+__version_tuple__ = version_tuple = (0, 0, "post48", "g00a2786c9.d20260802")
 
-__commit_id__ = commit_id = "ga8284f6c5"
+__commit_id__ = commit_id = "g00a2786c9"
