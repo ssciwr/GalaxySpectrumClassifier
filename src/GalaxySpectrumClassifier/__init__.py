373e7707868d0f7e8aa168c34632345d4c376a94
Harald Mack <harald.mack@protonmail.com>
2026-08-28 16:52:51 +0200

    remove inference classes b/c they are superfluous

--- src/GalaxySpectrumClassifier/__init__.py
+++ src/GalaxySpectrumClassifier/__init__.py
@@ -4,13 +4,14 @@ from .data import TabularDataset
 from .utils import to_xy
 from .simple_trainer import SimpleTrainer
 from .epoch_trainer import EpochTrainer
-from .inference import ClassifierInference, RegressionInference
+from .model_loading import load_default, load_skops, load_torch
 
 __all__ = [
     "TabularDataset",
     "SimpleTrainer",
     "to_xy",
     "EpochTrainer",
-    "ClassifierInference",
-    "RegressionInference",
+    "load_default",
+    "load_skops",
+    "load_torch",
 ]
